82671f252510df2dd2e15d3da616a6b9deaef878
AkiiiOO <152166237+AkiiiOO@users.noreply.github.com>
2024-09-08 01:41:37 +0800

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -12,7 +12,6 @@ CREATE TABLE Users (UserID INT PRIMARY KEY IDENTITY(1,1),
 					Password VARCHAR(255) NOT NULL, 
 					FirstName VARCHAR(100),
 					LastName VARCHAR(100),
-					Email VARCHAR(200) UNIQUE,
 					Contact_No Char(11), 
 					StatusID INT, 
 					CreatedDate DATETIME DEFAULT GETDATE(),
@@ -27,11 +26,14 @@ CREATE TABLE UserAccessLevels (UserAccessLevelID INT PRIMARY KEY IDENTITY(1,1),
 							   FOREIGN KEY (AccessLevelID) REFERENCES AccessLevels(AccessLevelID),
 							   UNIQUE(UserID, AccessLevelID));
 
+
+--changed added User ID fix tommorow
 CREATE TABLE AccessLevelPermissions (AccessLevelPermissionID INT PRIMARY KEY IDENTITY(1,1),
 									 AccessLevelID INT,
 									 PermissionID INT,
 									 FOREIGN KEY (AccessLevelID) REFERENCES AccessLevels(AccessLevelID),
-									 FOREIGN KEY (PermissionID) REFERENCES Permissions(PermissionID),);
+									 FOREIGN KEY (PermissionID) REFERENCES Permissions(PermissionID));
+									 
 
 
 INSERT INTO UserStatus (StatusID, StatusName) VALUES (1, 'Active');
@@ -53,8 +55,8 @@ INSERT INTO Permissions (PermissionName) VALUES ('Write');
 SELECT * FROM Permissions
 
 --Users
-INSERT INTO Users (Username, Password, FirstName, LastName, Email, Contact_No, StatusID)
-VALUES ('admin', 'admin', 'John', 'A', 'admin@gmail.com', '09345678911', 1);
+INSERT INTO Users (Username, Password, FirstName, LastName, Contact_No, StatusID)
+VALUES ('admin', 'admin', 'John', 'A', '09345678911', 1);
 
 --UserAccessLevels
 INSERT INTO UserAccessLevels (UserID, AccessLevelID) VALUES (1, 1);
@@ -70,11 +72,66 @@ CREATE TABLE Clients (ClientID INT IDENTITY(1,1) PRIMARY KEY,
 					  FirstName VARCHAR(100),
 					  MiddleInitial CHAR(1),
 					  LastName VARCHAR(100),
-					  Email VARCHAR(100),
 					  Address VARCHAR(200),
 					  Contact_No Char(11),
 					  DateCreated DATETIME DEFAULT GETDATE(),
 					  CreatedBy VARCHAR(200),
 					  FOREIGN KEY (UserID) REFERENCES Users (UserID));
-
 SELECT * FROM Clients
+
+--FOr the casket
+CREATE TABLE CasketType (CasketTypeID INT IDENTITY(1,1) PRIMARY KEY,
+						 CasketTypeName VARCHAR(50));
+
+INSERT INTO CasketType (CasketTypeName) VALUES ('Ordinary'),
+											  ('Semi-ordinary'),
+											  ('First-class ');
+SELECT * FROM CasketType
+
+CREATE TABLE Casket (CasketID INT IDENTITY(1,1) PRIMARY KEY,
+					 CasketTypeID INT FOREIGN KEY REFERENCES CasketType(CasketTypeID),
+					 CasketImage IMAGE,
+					 CasketName VARCHAR(100),
+					 Price DECIMAL(10, 2));	
+INSERT INTO Casket (CasketTypeID, CasketImage, CasketName, Price)VALUES (1, NULL, 'Classic Oak Casket', 600.00),
+																		(2, NULL, 'Stainless Steel Casket', 900.00);
+																		
+--For the Vehicle
+CREATE TABLE VehicleType (VehicleTypeID INT IDENTITY(1,1) PRIMARY KEY,
+						  VehicleTypeName VARCHAR(50));
+						  
+INSERT INTO VehicleType (VehicleTypeName) VALUES ('Hearse'),
+												 ('Van');
+SELECT * FROM VehicleType
+
+CREATE TABLE Vehicle (VehicleID INT IDENTITY(1,1) PRIMARY KEY,
+					  VehicleTypeID INT FOREIGN KEY REFERENCES VehicleType(VehicleTypeID),
+					  VehicleImage IMAGE,
+					  VehicleName VARCHAR(100),
+					  Price DECIMAL(10, 2));
+					  
+INSERT INTO Vehicle (VehicleTypeID, VehicleImage, VehicleName, Price) VALUES (1, NULL, 'Luxury Hearse', 250.00),
+																			 (2, NULL, 'Standard Van', 150.00);
+					  
+-- For the Flower Arrangement
+CREATE TABLE FlowerArrangementsType (ArrangementTypeID INT IDENTITY(1,1) PRIMARY KEY,
+									 ArrangementTypeName VARCHAR(50));
+									 
+INSERT INTO FlowerArrangementsType (ArrangementTypeName)VALUES ('Bouquet'),
+															   ('Wreath');
+									 
+CREATE TABLE FlowerArrangements (ArrangementID INT IDENTITY(1,1) PRIMARY KEY,
+							 	 ArrangementTypeID INT FOREIGN KEY REFERENCES FlowerArrangementsType(ArrangementTypeID),
+								 ArrangementImage IMAGE,
+							 	 ArrangementName VARCHAR(50),
+								 Price DECIMAL(10, 2));
+								 
+INSERT INTO FlowerArrangements (ArrangementTypeID, ArrangementImage, ArrangementName, Price) VALUES (1, NULL, 'White Sympathy Floor', 45.00),
+																								    (2, NULL, 'Tulip Centerpiece', 55.00);
+SELECT * FROM FlowerArrangements
+
+
+CREATE TABLE Package (PackageID INT IDENTITY(1,1) PRIMARY KEY,
+				  	  PackageName VARCHAR(100),
+					  Description TEXT,
+					  Price DECIMAL(10, 2));
